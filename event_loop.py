510308ec42eeaac49d9a9808635d1da8e94ead79
Brian Bui <iambrianbui@csu.fullerton.edu>
2018-10-05 11:08:45 -0700

    updated mazefile

--- event_loop.py
+++ event_loop.py
@@ -7,7 +7,8 @@ class EventLoop:
     def __init__(self, status):
         self.finished = status
 
-    def check_events(self):
+    @staticmethod
+    def check_events():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
